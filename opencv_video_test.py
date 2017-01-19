a1f3837abfc97bdc222ddc69abbef4f45e633cb4
Burke Ramsey <birk5437@gmail.com>
2017-01-18 21:10:06 -0500

    runs way faster

--- opencv_video_test.py
+++ opencv_video_test.py
@@ -1,6 +1,6 @@
 import numpy as np
 import cv2
-from matplotlib import pyplot as plt
+# from matplotlib import pyplot as plt
 
 # rgb_min_blue = np.uint8([[[27, 60, 40]]])
 # rgb_max_blue = np.uint8([[[81,255,120 ]]])
@@ -13,54 +13,86 @@ def nothing(x):
     pass
 hmin = 40
 hmax = 150
+
+smin = 40
+smax = 255
+
+vmin = 150
+vmax = 255
+
 #blue
 # hsv_min = np.array([110,50,50])
 # hsv_max = np.array([130,255,255])
 
 
-cap = cv2.VideoCapture(1)
+contours_arg_1 = 0
+contours_arg_3 = 3
+
+cv2.namedWindow("controls", cv2.WINDOW_NORMAL | cv2.WINDOW_OPENGL)
+cv2.createTrackbar('Hmin','controls', hmin, 255, nothing)
+cv2.createTrackbar('Hmax','controls', hmax, 255, nothing)
+cv2.createTrackbar('Smin','controls', smin, 255, nothing)
+cv2.createTrackbar('Smax','controls', smax, 255, nothing)
+cv2.createTrackbar('Vmin','controls', vmin, 255, nothing)
+cv2.createTrackbar('Vmax','controls', vmax, 255, nothing)
+cv2.createTrackbar('CountoursArg1','controls', contours_arg_1, 25, nothing)
+cv2.createTrackbar('CountoursArg3','controls', contours_arg_3, 25, nothing)
+
+
+cap = cv2.VideoCapture(0)
+# ret, img = cap.read()
+# small = cv2.resize(img, (0,0), fx=0.5, fy=0.5)
+# cv2.imshow('controls',small)
+
 while(True):
 
-    hsv_min = np.array([hmin,40,150])
-    hsv_max = np.array([hmax,255,260])
+    hsv_min = np.array([hmin,smin,vmin])
+    hsv_max = np.array([hmax,smax,vmax])
     ret, img = cap.read()
     # gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     hsv_img = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
 
     mask = cv2.inRange(hsv_img, hsv_min, hsv_max)
-    greyscale_image = mask
+    # greyscale_image = mask
 
     masked_image = cv2.bitwise_and(img,img, mask= mask)
 
-    gray = cv2.cvtColor(masked_image,cv2.COLOR_BGR2GRAY)
-    gray = np.float32(gray)
-    corners = cv2.goodFeaturesToTrack(gray, 100, .35, 15)
-    if corners is None:
-        print("no corners!")
-    else:
-        corners = np.int0(corners)
-        for corner in corners:
-            x,y = corner.ravel()
-            # cv2.circle(masked_image,(x,y),5,255,-1)
+    # gray = cv2.cvtColor(masked_image,cv2.COLOR_BGR2GRAY)
+    # gray = np.float32(gray)
+    # corners = cv2.goodFeaturesToTrack(gray, 100, .35, 15)
+    # if corners is None:
+    #     print("no corners!")
+    # else:
+    #     corners = np.int0(corners)
+    #     for corner in corners:
+    #         x,y = corner.ravel()
+    #         cv2.circle(masked_image,(x,y),5,255,-1)
 
-    ret2, thresh = cv2.threshold(greyscale_image, 40,40,150)
+    ret2, thresh = cv2.threshold(mask, 40,40,150)
     im2, contours, hierarchy = cv2.findContours(thresh,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
-    cv2.drawContours(masked_image, contours, -1, (0,255,0), 3)
+    cv2.drawContours(masked_image, contours, contours_arg_1 - 1, (0,255,0), contours_arg_3)
 
 
     # imshow doesnt work on mac for some reason
 
     # cv2.rectangle(masked_image,(15,20),(70, 50), ( 0, 55, 255), 2)
 
-    cv2.imshow('frame',masked_image)
+    small = cv2.resize(masked_image, (0,0), fx=0.5, fy=0.5)
+    cv2.imshow('frame',small)
     # plt.imshow(gray)
     # plt.show()
-    cv2.createTrackbar('Hmin','frame', hmin, 255, nothing)
-    hmin = cv2.getTrackbarPos ('Hmin', 'frame')
-    print(hmin)
-    cv2.createTrackbar('Hmax','frame', hmax, 255, nothing)
-    hmax = cv2.getTrackbarPos ('Hmax', 'frame')
-    print(hmax)
+
+
+    hmin = cv2.getTrackbarPos ('Hmin', 'controls')
+    # print(hmin)
+    hmax = cv2.getTrackbarPos ('Hmax', 'controls')
+    # print(hmax)
+    smin = cv2.getTrackbarPos ('Smin', 'controls')
+    smax = cv2.getTrackbarPos ('Smax', 'controls')
+    vmin = cv2.getTrackbarPos ('Vmin', 'controls')
+    vmax = cv2.getTrackbarPos ('Vmax', 'controls')
+    contours_arg_1 = cv2.getTrackbarPos ('CountoursArg1', 'controls')
+    contours_arg_3 = cv2.getTrackbarPos ('CountoursArg3', 'controls')
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
