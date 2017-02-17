065a98bb285b3035ee34cc90ab57d92cc804d57e
Burke Ramsey <birk5437@gmail.com>
2017-02-17 18:47:19 -0500

    spaces to tabs

--- the_real_vision_detector.py
+++ the_real_vision_detector.py
@@ -9,15 +9,15 @@ import cv2
 # green tape
 
 def nothing(x):
-    pass
+	pass
 
 
 def width_and_height_from_contour(contour, image_to_draw_on, approximation_value):
-    epsilon = approximation_value * cv2.arcLength(contour, True)
-    approx = cv2.approxPolyDP(contour, epsilon, True)
-    x,y,w,h = cv2.boundingRect(approx)
-    # cv2.rectangle(image_to_draw_on,(x,y),(x+w, y+h), (0,0,255), 4)
-    return [w, h]
+	epsilon = approximation_value * cv2.arcLength(contour, True)
+	approx = cv2.approxPolyDP(contour, epsilon, True)
+	x,y,w,h = cv2.boundingRect(approx)
+	# cv2.rectangle(image_to_draw_on,(x,y),(x+w, y+h), (0,0,255), 4)
+	return [w, h]
 
 
 
@@ -25,53 +25,53 @@ def width_and_height_from_contour(contour, image_to_draw_on, approximation_value
 
 def draw_bounding_rectangle(image_to_draw_on, contours, approximation_value):
 
-    the_thing_we_want = contours[0]
-    lowest_difference = 99999999999999999999999
+	the_thing_we_want = contours[0]
+	lowest_difference = 99999999999999999999999
 
-    differences_with_contours = []
+	differences_with_contours = []
 
-    for found_contour in contours:
-        width_and_height = width_and_height_from_contour(found_contour, image_to_draw_on, approximation_value)
+	for found_contour in contours:
+		width_and_height = width_and_height_from_contour(found_contour, image_to_draw_on, approximation_value)
 
-        difference = abs((width_and_height[0] / width_and_height[1]) - (2 / 5))
-        differences_with_contours.append([difference, found_contour])
+		difference = abs((width_and_height[0] / width_and_height[1]) - (2 / 5))
+		differences_with_contours.append([difference, found_contour])
 
-    differences_with_contours = sorted(differences_with_contours, key=lambda x: x[0])
-    # print "differences_with_contours"
-    # print differences_with_contours
+	differences_with_contours = sorted(differences_with_contours, key=lambda x: x[0])
+	# print "differences_with_contours"
+	# print differences_with_contours
 
-    if len(differences_with_contours) > 1:
-        for arr in [differences_with_contours[0], differences_with_contours[1]]:
-    # for arr in differences_with_contours:
-          epsilon = approximation_value * cv2.arcLength(arr[1], True)
-          approx = cv2.approxPolyDP(arr[1], epsilon, True)
-          x,y,w,h = cv2.boundingRect(approx)
-          cv2.rectangle(image_to_draw_on,(x,y),(x+w, y+h), (0,0,255), 4)
+	if len(differences_with_contours) > 1:
+		for arr in [differences_with_contours[0], differences_with_contours[1]]:
+	# for arr in differences_with_contours:
+		  epsilon = approximation_value * cv2.arcLength(arr[1], True)
+		  approx = cv2.approxPolyDP(arr[1], epsilon, True)
+		  x,y,w,h = cv2.boundingRect(approx)
+		  cv2.rectangle(image_to_draw_on,(x,y),(x+w, y+h), (0,0,255), 4)
 
-    print "width: "
-    print width_and_height_from_contour(the_thing_we_want, image_to_draw_on, approximation_value)[0]
-    print "height: "
-    print width_and_height_from_contour(the_thing_we_want, image_to_draw_on, approximation_value)[1]
+	print "width: "
+	print width_and_height_from_contour(the_thing_we_want, image_to_draw_on, approximation_value)[0]
+	print "height: "
+	print width_and_height_from_contour(the_thing_we_want, image_to_draw_on, approximation_value)[1]
 
-    # largest_contour = contours[0]
-    # for found_contour in contours:
+	# largest_contour = contours[0]
+	# for found_contour in contours:
 
-    #     if cv2.contourArea(found_contour) < cv2.contourArea(largest_contour):
-    #         largest_contour = found_contour
-
-
-
-    # print cv2.contourArea(largest_contour)
-    # epsilon = approximation_value * cv2.arcLength(largest_contour, True)
-    # approx = cv2.approxPolyDP(largest_contour, epsilon, True)
-    # # cv2.drawContours(image_to_draw_on, [approx], -1, (255,0,0), 4)
-    # if len(approx) == 4:
-    #     x,y,w,h = cv2.boundingRect(approx)
-    #     cv2.rectangle(image_to_draw_on,(x,y),(x+w, y+h), (0,0,255), 4)
+	#	 if cv2.contourArea(found_contour) < cv2.contourArea(largest_contour):
+	#		 largest_contour = found_contour
 
 
 
-    # cv2.drawContours(image_to_draw_on, [found_contour], -1, (0,255,0), 4) 
+	# print cv2.contourArea(largest_contour)
+	# epsilon = approximation_value * cv2.arcLength(largest_contour, True)
+	# approx = cv2.approxPolyDP(largest_contour, epsilon, True)
+	# # cv2.drawContours(image_to_draw_on, [approx], -1, (255,0,0), 4)
+	# if len(approx) == 4:
+	#	 x,y,w,h = cv2.boundingRect(approx)
+	#	 cv2.rectangle(image_to_draw_on,(x,y),(x+w, y+h), (0,0,255), 4)
+
+
+
+	# cv2.drawContours(image_to_draw_on, [found_contour], -1, (0,255,0), 4) 
 
 hmin = 40
 hmax = 150
@@ -101,54 +101,54 @@ cv2.createTrackbar('Vmin','controls', vmin, 255, nothing)
 cv2.createTrackbar('Vmax','controls', vmax, 255, nothing)
 
 
-cap = cv2.VideoCapture(0)
+cap = cv2.VideoCapture(1)
 ret, img = cap.read()
 small = cv2.resize(img, (0,0), fx=0.5, fy=0.5)
 cv2.imshow('controls',small)
 
 while(True):
 
-    hsv_min = np.array([hmin,40,150])
-    hsv_max = np.array([hmax,255,260])
-    ret, img = cap.read()
-    # gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    hsv_img = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+	hsv_min = np.array([hmin,40,150])
+	hsv_max = np.array([hmax,255,260])
+	ret, img = cap.read()
+	# gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+	hsv_img = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
 
-    mask = cv2.inRange(hsv_img, hsv_min, hsv_max)
-    while (blur_factor % 2 != 1):
-        blur_factor += 1
-    mask = cv2.medianBlur(mask,blur_factor)
-    greyscale_image = mask
+	mask = cv2.inRange(hsv_img, hsv_min, hsv_max)
+	while (blur_factor % 2 != 1):
+		blur_factor += 1
+	mask = cv2.medianBlur(mask,blur_factor)
+	greyscale_image = mask
 
-    masked_image = cv2.bitwise_and(img,img, mask= mask)
+	masked_image = cv2.bitwise_and(img,img, mask= mask)
 
-    ret2, thresh = cv2.threshold(greyscale_image, 40,40,150)
-    im2, contours, hierarchy = cv2.findContours(thresh,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
-    #cv2.drawContours(masked_image, contours, -1, (0,255,0), 3)
-    draw_bounding_rectangle(masked_image, contours, .010)
+	ret2, thresh = cv2.threshold(greyscale_image, 40,40,150)
+	im2, contours, hierarchy = cv2.findContours(thresh,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
+	#cv2.drawContours(masked_image, contours, -1, (0,255,0), 3)
+	draw_bounding_rectangle(masked_image, contours, .010)
 
 
-    # imshow doesnt work on mac for some reason
+	# imshow doesnt work on mac for some reason
 
-    # cv2.rectangle(masked_image,(15,20),(70, 50), ( 0, 55, 255), 2)
+	# cv2.rectangle(masked_image,(15,20),(70, 50), ( 0, 55, 255), 2)
 
-    small = cv2.resize(masked_image, (0,0), fx=0.5, fy=0.5)
-    small2 = cv2.resize(img, (0,0), fx=0.5, fy=0.5)
-    small3 = cv2.resize(mask, (0,0), fx=0.5, fy=0.5)
-    cv2.imshow('frame',small)
-    # cv2.imshow('frame2',small2)
-    # cv2.imshow('frame3',small3)
+	small = cv2.resize(masked_image, (0,0), fx=0.5, fy=0.5)
+	small2 = cv2.resize(img, (0,0), fx=0.5, fy=0.5)
+	small3 = cv2.resize(mask, (0,0), fx=0.5, fy=0.5)
+	cv2.imshow('frame',small)
+	# cv2.imshow('frame2',small2)
+	# cv2.imshow('frame3',small3)
 
-    blur_factor = cv2.getTrackbarPos ('blur_factor', 'controls')
-    hmin = cv2.getTrackbarPos ('Hmin', 'controls')
-    hmax = cv2.getTrackbarPos ('Hmax', 'controls')
-    smin = cv2.getTrackbarPos ('Smin', 'controls')
-    smax = cv2.getTrackbarPos ('Smax', 'controls')
-    vmin = cv2.getTrackbarPos ('Vmin', 'controls')
-    vmax = cv2.getTrackbarPos ('Vmax', 'controls')
+	blur_factor = cv2.getTrackbarPos ('blur_factor', 'controls')
+	hmin = cv2.getTrackbarPos ('Hmin', 'controls')
+	hmax = cv2.getTrackbarPos ('Hmax', 'controls')
+	smin = cv2.getTrackbarPos ('Smin', 'controls')
+	smax = cv2.getTrackbarPos ('Smax', 'controls')
+	vmin = cv2.getTrackbarPos ('Vmin', 'controls')
+	vmax = cv2.getTrackbarPos ('Vmax', 'controls')
 
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
+	if cv2.waitKey(1) & 0xFF == ord('q'):
+		break
 
 cap.release()
 cv2.destroyAllWindows()
